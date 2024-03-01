7e62210db1c6e50a386f3e364694c542cb3bb2c4
Bechuana1 <mykbechuana@gmail.com>
2024-03-01 22:48:39 +0300

    file complete

---
+++ control-stt.php
@@ -0,0 +1,88 @@
+<?php
+
+// $age = 17;
+
+// if ($age >= 18){
+//     echo 'you are eligible to vote';
+// }else{
+//     echo 'wait for next elections';
+// }
+
+
+// $grade = 95;
+
+// if ($grade >= 90){
+//     echo 'Grade A';
+// }
+// else if($grade >= 80){
+//     echo 'Grade B';
+// }else if($grade >= 70){
+//     echo 'Grade C';
+// }
+// else{
+//     echo 'Grade D';
+// }
+
+// $day  = "Mon";
+
+// switch ($day){
+//     case "Mon":
+//         echo 'Its A happy monday';
+//         break;
+    
+//     case "Tue":
+//         echo 'its book club Tuesday';
+//         break;
+    
+//     case "Thurs":
+//         echo 'is Fish Day';
+//         break;
+//     case "fri":
+//         echo 'Fun Fact:  Friday is named after the Norse goddess Freya, goddess of love, beauty adn fertility. Equivalent to Venus in Roman Mythology';
+//         break;
+
+//     default:
+//         echo 'Grade D';
+// }
+
+
+// $is_logged_in = true;
+// if($is_logged_in){
+//     echo 'welcome to the members area';
+// }else{
+//     echo 'please log in to continue';
+// }
+
+
+
+// if ($username == "admin" && $password == "secret123") {
+//     // Successful login 
+// } else {
+//     // Display an error message
+// }
+
+
+// LOOPs
+
+// for($i = 0; $i < 10 ; $i++){
+//     echo $i;
+// }
+
+$i = 0;
+// while($i <10){
+//     echo $i;
+//     $i++;
+// }
+
+do{
+    echo $i;
+    $i++;
+} while ($i <10);
+
+
+
+
+
+
+
+?>
