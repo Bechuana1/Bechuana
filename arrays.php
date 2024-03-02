1891875c927a0904f35bbad32fc47c5d02055d35
Bechuana1 <mykbechuana@gmail.com>
2024-03-02 07:26:57 +0300

    arrays made

---
+++ arrays.php
@@ -0,0 +1,26 @@
+<?php
+
+$foo = array();
+
+
+$foo = [];
+
+
+// $fruits = array('apples', 'blue berries', 'oranges');
+
+// // echo json_encode($fruits);
+// var_dump($fruits);
+
+// // print_r($fruits);
+
+
+//initializing using custom indexes
+
+$fruit = [
+    'first' =>'pears',
+    'second' => 'grapes',
+    'third' => 'oranges'
+];
+
+
+print_r($fruit);
---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php // this file is all about functions
+
+// $name = 'mike';
+
+// function greet($name){
+//     echo 'hello world' . $name;
+// }
+
+// //  greet('Mike');
+
+// var_dump($name);
+
+// // print_r($name);
+
+// function loop($i){
+//     if ($i <10){
+//         echo $i;
+//         $i++;
+//     }
+    
+// }
+
+// loop(0);
+
+
+function validateUser($username, $email, $password){
+    $error = [];
+}
